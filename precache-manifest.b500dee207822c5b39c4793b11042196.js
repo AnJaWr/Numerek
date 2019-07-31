3e9fede1d4738db38e2733c9d5fd1e7da511e2b5
AnJaWr <october3@o2.pl>
2019-07-31 15:59:38 +0200

    Updates

---
+++ precache-manifest.b500dee207822c5b39c4793b11042196.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde1f772514887132a68e9f9949560c2",
+    "url": "/Numerek/index.html"
+  },
+  {
+    "revision": "299a57bc64d9d7a8fbd1",
+    "url": "/Numerek/static/css/main.4331f015.chunk.css"
+  },
+  {
+    "revision": "5049fb4898bdd6aca9cd",
+    "url": "/Numerek/static/js/2.16a1f3ed.chunk.js"
+  },
+  {
+    "revision": "299a57bc64d9d7a8fbd1",
+    "url": "/Numerek/static/js/main.566c5f9d.chunk.js"
+  },
+  {
+    "revision": "4f2868c471eb328b78a3",
+    "url": "/Numerek/static/js/runtime~main.401f2ba4.js"
+  },
+  {
+    "revision": "46b121600ce95b332c2001a03da78a29",
+    "url": "/Numerek/static/media/Schopenhauer.46b12160.jpg"
+  },
+  {
+    "revision": "c55d200ab1fdc3329132126817c259c0",
+    "url": "/Numerek/static/media/coldnightforalligators.c55d200a.ttf"
+  }
+]);
